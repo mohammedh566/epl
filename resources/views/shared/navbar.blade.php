6483e76ad097fd6694fe3b80ccd2930892aeddd3
mohammed hamza <mohammedh566@gmail.com>
2016-12-21 14:45:16 +0000

    Logout functionality completed

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -14,17 +14,30 @@
         <!-- Navbar Right -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="/">Home</a></li>
+                <li class="active"><a href="{!! action('HomeController@index') !!}">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Member 
-                    <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="/register">Register</a></li>
-                        <li><a href="/login">Login</a></li>
-                    </ul>
-                </li>
+                @if (Auth::guest())
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Member 
+                        <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="/login">Login</a></li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} 
+                        <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="/logout" onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">Logout</a></li>
+                             <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </ul>
+                    </li>
+                @endif
             </ul>
         </div>
     </div>
